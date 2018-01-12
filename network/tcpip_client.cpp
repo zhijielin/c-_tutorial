b75ba24d259180e2094189eedb688df91e4b7e68
ZHIJIE LIN <zhijielin@eduroam-ipv4-0494.triple-a.uni-kl.de>
2018-01-12 15:01:13 +0100

    network client server

--- network/tcpip_client.cpp
+++ network/tcpip_client.cpp
@@ -1,28 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-#include <iostream>
-#include "TCPClient.h"
-
-int main(int argc, char *argv[])
-{
-    if(argc >= 2)
-    {
-        TCPClient tcp;
-        tcp.setup("127.0.0.1",11999);
-        int num = atoi(argv[2]);
-        cout << "Num Request:" << num <<endl;
-        for(int i = 0; i < num; i++)
-        {
-            string msg = argv[1];
-            tcp.Send(msg);
-            string rec = tcp.receive();
-            if( rec != "" )
-            {
-                cout << "Server Response:" << rec << endl;
-            }
-            sleep(1);
-        }
-        exit(0);
-        return 0;
-    }
-    else cout << "Error: message num-request" << endl;
+int main(){
+    //创建套接字
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    
+    //向服务器（特定的IP和端口）发起请求
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
+    serv_addr.sin_family = AF_INET;  //使用IPv4地址
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
+    serv_addr.sin_port = htons(1234);  //端口
+    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    //读取服务器传回的数据
+    char buffer[40];
+    read(sock, buffer, sizeof(buffer)-1);
+    
+    printf("Message form server: %s\n", buffer);
+    
+    //关闭套接字
+    close(sock);
+    
+    return 0;
 }
